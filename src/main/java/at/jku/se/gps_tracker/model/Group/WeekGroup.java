29fb75f7848cd5422f2bb7a0d8d3cdf996537872
nvrxy <37603873+nvrxy@users.noreply.github.com>
2022-05-26 02:41:05 +0200

    added time frame for week number name

--- src/main/java/at/jku/se/gps_tracker/model/Group/WeekGroup.java
+++ src/main/java/at/jku/se/gps_tracker/model/Group/WeekGroup.java
@@ -1,22 +1,45 @@
 package at.jku.se.gps_tracker.model.Group;
 
-import java.time.Year;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class WeekGroup extends GroupTrack {
 
     private int week;
 
 
-    public WeekGroup(int week)
+    public WeekGroup(int week, int year)
     {
         super();
         this.week = week;
+        super.year = year;
         setName();
     }
+    public Calendar getFirstDayOfWeek(int week) {
 
+        int year = super.getYear();
+        int day = 1; //monday = first day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setWeekDate(year, week, 1);
+
+        return calendar;
+
+    }
+
+    public Calendar getLastDayOfWeek(int week) {
+
+        Calendar calendar = getFirstDayOfWeek(week);
+        calendar.add(Calendar.DAY_OF_MONTH, 7);
+        return calendar;
+
+    }
 
     public void setName() {
-        super.setName("W:" + week);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM");
+        super.setName("w" + week + " (" + sdf.format(getFirstDayOfWeek(week).getTime()) + " - " + sdf.format(getLastDayOfWeek(week).getTime()) + "."+ getYear() + ")");
     }
 
     @Override
